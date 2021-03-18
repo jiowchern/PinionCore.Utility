0dd8d41feafa73cd4206e3097f4722dac4b3975d
jc <jiowchern@gmail.com>
2021-03-18 17:28:38 +0800

    simplify.

--- Regulus.Utility/Utility/Poller.cs
+++ Regulus.Utility/Utility/Poller.cs
@@ -5,54 +5,39 @@ namespace Regulus.Utility
 {
     public class Poller<T> where T : class
     {
-        private readonly Queue<T> _Adds = new Queue<T>();
+        
+        private readonly List<T> _Objects;
+        
+        
 
-        private readonly List<T> _Objects = new List<T>();
 
-        private readonly Queue<Func<T, bool>> _Removes = new Queue<Func<T, bool>>();
-
+        public Poller()
+        {
+            _Objects = new List<T>();
+            
+        }
         public void Add(T obj)
         {
-            lock (_Adds)
-                _Adds.Enqueue(obj);
+
+            lock (_Objects)
+                _Objects.Add(obj);
         }
 
-        public void Remove(Func<T, bool> obj)
+        public void Remove(Func<T, bool> predicate)
         {
-            lock (_Removes)
-                _Removes.Enqueue(obj);
+            lock (_Objects)
+                _Objects.RemoveAll((o) => predicate(o));
+
         }
 
         public T[] UpdateSet()
         {
             lock (_Objects)
             {
-                lock (_Adds)
-                    _Add(_Adds);
-                lock (_Removes)
-                    _Remove(_Removes);
-
                 return _Objects.ToArray();
             }
         }
 
-        private void _Remove(Queue<Func<T, bool>> removes)
-        {
-            while (removes.Count > 0)
-            {
-                Func<T, bool> obj = removes.Dequeue();
-
-                _Objects.RemoveAll(o => obj.Invoke(o));
-            }
-        }
-
-        private void _Add(Queue<T> adds)
-        {
-            while (adds.Count > 0)
-            {
-                T obj = adds.Dequeue();
-                _Objects.Add(obj);
-            }
-        }
+      
     }
 }
