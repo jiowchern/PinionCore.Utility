8479609c28cc2b078df33a12bfc7c7b1d3fa70d8
jc <jiowchern@gmail.com>
2021-05-21 07:00:43 +0800

    awaitable remote value .

---
+++ Regulus.Utility/Remote/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Regulus.Remote
+{
+    public static class Helper
+    {
+        public static Action<Value<T>> UnBox<T>(Action<T> callback)
+        {
+            return val => { val.OnValue += callback; };
+        }
+
+        public static T Result<T>(this Value<T> value)
+        {
+            WaitHandle handle = new AutoResetEvent(false);
+            ValueWaiter<T> valueSpin = new ValueWaiter<T>(value);
+            ThreadPool.QueueUserWorkItem(valueSpin.Run, handle);
+            WaitHandle.WaitAll(
+                new[]
+                {
+                    handle
+                });
+            return valueSpin.Value;
+        }
+    }
+}
--- Regulus.Utility/Remote/Value.cs
+++ Regulus.Utility/Remote/Value.cs
@@ -1,53 +1,18 @@
 ﻿using System;
-using System.Threading;
+using System.Runtime.CompilerServices;
 
 namespace Regulus.Remote
 {
-    public interface IValue
-    {
-        object GetObject();
-
-        void SetValue(object val);
-
-        void QueryValue(Action<object> action);
-
-        void SetValue(IGhost ghost);
-
-        bool IsInterface();
-
-        Type GetObjectType();
-
-    }
-
-    public static class Helper
-    {
-        public static Action<Value<T>> UnBox<T>(Action<T> callback)
-        {
-            return val => { val.OnValue += callback; };
-        }
-
-        public static T Result<T>(this Value<T> value)
-        {
-            WaitHandle handle = new AutoResetEvent(false);
-            ValueWaiter<T> valueSpin = new ValueWaiter<T>(value);
-            ThreadPool.QueueUserWorkItem(valueSpin.Run, handle);
-            WaitHandle.WaitAll(
-                new[]
-                {
-                    handle
-                });
-            return valueSpin.Value;
-        }
-    }
 
 
     /// <summary>
     ///     接收或傳送遠端來的資料
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public sealed class Value<T> : IValue 
+    public sealed class Value<T> : IValue ,  IAwaitable<T>
     {
         private event Action<T> _OnValue;
+        Action _Continuation;
 
         /// <summary>
         ///     如果有設定資料則會發生此事件
@@ -67,35 +32,30 @@ namespace Regulus.Remote
             remove { _OnValue -= value; }
         }
 
+        
+
         private readonly bool _Interface;
 
-        private bool _Empty = true;
+        private bool _Empty;
 
         private T _Value;
 
-        /// <summary>
-        ///     空物件
-        /// </summary>
+        
         public static Value<T> Empty
         {
             get { return default(T); }
         }
 
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="Value{T}" /> class.
-        ///     建構子
-        /// </summary>
+        bool IAwaitable<T>.IsCompleted => HasValue();
+
         public Value()
         {
+            _Continuation = () => { };
+            _Empty = true;
             _Interface = typeof(T).IsInterface;
         }
 
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="Value{T}" /> class.
-        ///     預設已經填入資料
-        /// </summary>
-        /// <param name="val">
-        /// </param>
+        
         public Value(T val) : this()
         {
             _Empty = false;
@@ -111,34 +71,11 @@ namespace Regulus.Remote
             return _Value;
         }
 
-        void IValue.SetValue(IGhost val)
+       
+
+        bool IValue.SetValue(object val)
         {
-            if (_Empty == false)
-            {
-                throw new Exception("重覆的set value");
-            }
-            _Empty = false;
-
-            _Value = (T)val;
-            if (_OnValue != null)
-            {
-                _OnValue(_Value);
-            }
-        }
-
-        void IValue.SetValue(object val)
-        {
-            if (_Empty == false)
-            {
-                throw new Exception("重覆的set value");
-            }
-            _Empty = false;
-
-            _Value = (T)val;
-            if (_OnValue != null)
-            {
-                _OnValue(_Value);
-            }
+            return SetValue((T)val);
         }
 
         void IValue.QueryValue(Action<object> action)
@@ -181,22 +118,22 @@ namespace Regulus.Remote
             return _Empty == false;
         }
 
-        /// <summary>
-        ///     設定資料，將會發生OnValue事件
-        /// </summary>
-        /// <param name="val"></param>
-        public void SetValue(T val)
+        
+        public bool SetValue(T val)
         {
             if (_Empty == false)
             {
-                throw new Exception("重覆的set value");
+                return false;
             }
             _Empty = false;
             _Value = val;
+            _Continuation();
             if (_OnValue != null)
             {
                 _OnValue(_Value);
             }
+            
+            return true;
         }
 
         /// <summary>
@@ -216,6 +153,24 @@ namespace Regulus.Remote
             return false;
         }
 
+        public IAwaitable<T> GetAwaiter()
+        {
+            return this;
+        }
 
+        bool IValue.SetValue(IGhost ghost)
+        {
+            return SetValue((T)ghost);
+        }
+
+        T IAwaitable<T>.GetResult()
+        {
+            return GetValue();
+        }
+
+        void INotifyCompletion.OnCompleted(Action continuation)
+        {
+            _Continuation = continuation;
+        }
     }
 }
